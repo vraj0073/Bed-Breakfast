9c9c6b5de42ab6523db09409f5c94ab0951928ff
Sparsh Purwar <its.touch@T98F2.WPA.Dal.Ca>
2022-07-17 20:27:06 -0300

    Adding pages for tour

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import {
 import Hotel from "./components/Hotel/Hotel";
 import BookRoom from "./components/Hotel/BookRoom";
 import Bookings from "./components/Hotel/Bookings";
+import Tour1 from "./components/Hotel/Tour1";
+import Tour2 from "./components/Hotel/Tour2";
+import Tour3 from "./components/Hotel/Tour3";
+import Tour4 from "./components/Hotel/Tour4";
+import Tour5 from "./components/Hotel/Tour5";
+
+
+
 
 function App() {
   return (
@@ -19,6 +27,11 @@ function App() {
             <Route exact path="/rooms" element={<Hotel />} />
             <Route exact path="/book" element={<BookRoom />} />
             <Route exact path="/bookings" element={<Bookings />} />
+            <Route exact path="/tour1" element={<Tour1 />} />
+            <Route exact path="/tour2" element={<Tour2 />} />
+            <Route exact path="/tour3" element={<Tour3 />} />
+            <Route exact path="/tour4" element={<Tour4 />} />
+            <Route exact path="/tour5" element={<Tour5 />} />
             {/* Change End */}
           </Routes>
         </Router>
